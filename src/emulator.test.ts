f23785f59a02a7f9883fc1c8a68d0b270de99483
Andrew Huth <andrew@huth.me>
2021-04-30 15:24:53 -0700

    Add better tests for using LDA with zero and "negative" numbers

--- src/emulator.test.ts
+++ src/emulator.test.ts
@@ -1,7 +1,7 @@
 import {CPU} from './emulator';
 
 describe('LDA - 0xA9', () => {
-  test('loading a byte into memory', () => {
+  test('loading a positive byte', () => {
     const cpu = new CPU();
     cpu.interpret([
       // LDA 0x05
@@ -20,7 +20,7 @@ describe('LDA - 0xA9', () => {
     expect(cpu.status & 0b1000_0000).toEqual(0);
   });
 
-  test('zero flag', () => {
+  test('loading zero', () => {
     const cpu = new CPU();
     cpu.interpret([
       // LDA 0x00
@@ -29,7 +29,32 @@ describe('LDA - 0xA9', () => {
       0x00,
     ]);
 
+    // 0x00 is in the accumulator.
+    expect(cpu.register_acc).toEqual(0x00);
+
     // Zero flag is set?
     expect(cpu.status & 0b0000_0010).toEqual(0b10);
+
+    // Negative flag is not set?
+    expect(cpu.status & 0b1000_0000).toEqual(0);
+  });
+
+  test('loading a negative byte', () => {
+    const cpu = new CPU();
+    cpu.interpret([
+      // LDA 0x88. 0x88 has the 0b1000_0000 bit set, so it is negative.
+      0xA9, 0x88,
+      // BRK
+      0x00,
+    ]);
+
+    // 0x88 is in the accumulator.
+    expect(cpu.register_acc).toEqual(0x88);
+
+    // Zero flag is not set?
+    expect(cpu.status & 0b0000_0010).toEqual(0b00);
+
+    // Negative flag is set?
+    expect(cpu.status & 0b1000_0000).not.toEqual(0);
   });
 });
