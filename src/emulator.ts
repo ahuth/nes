6621162a00bce048cb9105093f0b378f144b5d49
Andrew Huth <andrew@huth.me>
2021-05-03 16:27:53 -0700

    Load program into "memory" before executing

--- src/emulator.ts
+++ src/emulator.ts
@@ -22,15 +22,41 @@ export class CPU {
   status = 0b0000_0000;
   /** 16-bit program counter */
   program_counter = 0b0000_0000_0000_0000;
+  /**
+   * Memory.
+   *
+   * - 0x0000 to 0x2000 is RAM (the NES has 2KiB of RAM. By my math 0-0x2000 is only 1KiB. Not sure where the disconnect is)
+   * - 0x2000 to 0x4020 is redirected to other hardware modules (PPU, APU, GamePads, etc)
+   * - 0x4020 to 0x6000 is used by different generations of cartridges differently. May be mapped to RAM, ROM, or nothing at all. Maybe this is where the extra 1KiB of RAM comes from?
+   * - 0x6000 to 0x8000 is for RAM provided by the cartridge, if any.
+   * - 0x8000 to 0x10000 is mapped to program ROM on the cartridge.
+   *
+   * @see https://bugzmanov.github.io/nes_ebook/chapter_3.html
+   */
+  memory = new Array(0xFFFF).fill(0);
 
   /**
    * Run a program made up of instructions and any arguments to them.
    */
   interpret(program: number[]) {
-    this.program_counter = 0;
+    this.load(program);
+    this.run();
+  }
 
-    while (this.program_counter < program.length) {
-      const opCode = program[this.program_counter];
+  /**
+   * Load a program (from a cartridge) into the right place in memory.
+   */
+  private load(program: number[]) {
+    this.memory.splice(0x8000, program.length, ...program);
+    this.program_counter = 0x8000;
+  }
+
+  /**
+   * Run a program made up of instructions and any arguments to them.
+   */
+  private run() {
+    while (true) {
+      const opCode = this.memoryRead(this.program_counter);
       this.program_counter += 1;
 
       switch (opCode) {
@@ -39,7 +65,7 @@ export class CPU {
         // Load a byte of memory into the accumulator and set the zero and negative flags as
         // appropriate.
         case Instruction.LDA: {
-          const param = program[this.program_counter];
+          const param = this.memoryRead(this.program_counter);
           this.program_counter += 1;
           this.register_acc = param;
           this.updateZeroAndNegativeFlags(this.register_acc);
@@ -63,6 +89,24 @@ export class CPU {
           throw new Error('todo!');
       }
     }
+  }
+
+  /**
+   * Get data from memory.
+   * @param address 16-bit memory address
+   * @returns 8-bit data
+   */
+  private memoryRead(address: number): number {
+    return this.memory[address];
+  }
+
+  /**
+   * Set data in memory.
+   * @param address 16-bit memory address
+   * @param data 8-bit data
+   */
+  private memorySet(address: number, data: number): void {
+    this.memory[address] = data;
   }
 
   private updateZeroAndNegativeFlags(result: number): void {
