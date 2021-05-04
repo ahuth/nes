61bb5ee8681910f2676ab794bd0623db4188feb6
Andrew Huth <andrew@huth.me>
2021-05-03 21:06:04 -0700

    Add addressing modes and way to get the operand address based on an addressing mode

--- src/cpu.ts
+++ src/cpu.ts
@@ -1,4 +1,53 @@
 /**
+ * Addressing mode which determines how the CPU interprets an instruction and their parameters.
+ * @see http://www.obelisk.me.uk/6502/addressing.html
+ */
+export enum AddressingMode {
+  /** Parameters are values, not memory addresses. */
+  Immediate,
+  /** Parameters are a 8 bits (1 byte), so only the first 256 bytes can be used. */
+  ZeroPage,
+  /**
+   * Parameters are a 8 bits (1 byte). The value in the X register is added to them to
+   * get the final memory address that's used.
+   */
+  ZeroPage_X,
+  /**
+   * Parameters are a 8 bits (1 byte). The value in the Y register is added to them to
+   * get the final memory address that's used. Can only be used with the LDX and STX instructions.
+   */
+  ZeroPage_Y,
+  /** Parameters are a full 16-bit (2 byte) memory address. */
+  Absolute,
+  /**
+   * Parameters are a 16 bits (2 bytes). The value in the X register is added to them to
+   * get the final memory address that's used.
+   */
+  Absolute_X,
+  /**
+   * Parameters are a 16 bits (2 bytes). The value in the Y register is added to them to
+   * get the final memory address that's used.
+   */
+  Absolute_Y,
+  /**
+   * Also known as Indexed Indirect.
+   *
+   * Normally used in conjunction with a table of address held on zero page. The address of the
+   * table is taken from the instruction and the X register added to it (with zero page wrap
+   * around) to give the location of the least significant byte of the target address.
+   */
+  Indirect_X,
+  /**
+   * Also known as Indirect Indexed.
+   *
+   * The most common indirection mode used on the 6502. An instruction contains the zero page
+   * location of the least significant byte of 16 bit address. The Y register is dynamically added
+   * to this value to generated the actual target address for operation.
+   */
+  Indirect_Y,
+}
+
+/**
  * Assembly instructions for the NES's 2A03 chip (which is a modified version of the 6502).
  * @see http://www.obelisk.me.uk/6502/reference.html
  */
@@ -18,6 +67,8 @@ export class CPU {
   register_acc = 0b0000_0000;
   /** 8-bit X register */
   register_x = 0b0000_0000;
+  /** 8-bit Y register */
+  register_y = 0b0000_0000;
   /** 8-bit processor status */
   status = 0b0000_0000;
   /** 16-bit program counter */
@@ -60,6 +111,7 @@ export class CPU {
   private reset() {
     this.register_acc = 0;
     this.register_x = 0;
+    this.register_y = 0;
     this.status = 0;
     this.program_counter = this.memoryReadUint16(0xFFFC);
   }
@@ -147,6 +199,66 @@ export class CPU {
     const lo = data & 0xFF;
     this.memoryWrite(address, lo);
     this.memoryWrite(address + 1, hi);
+  }
+
+  /**
+   * Get the address of an instruction's parameter, which depends on its addressing mode.
+   * @returns 16-bit memory address
+   */
+  private getOperandAddress(mode: AddressingMode): number {
+    switch (mode) {
+      case AddressingMode.Immediate:
+        return this.program_counter;
+      case AddressingMode.ZeroPage:
+        return this.memoryRead(this.program_counter);
+      case AddressingMode.Absolute:
+        return this.memoryReadUint16(this.program_counter);
+      case AddressingMode.ZeroPage_X: {
+        const specifiedAddress = this.memoryRead(this.program_counter);
+        const offset = this.register_x;
+        // Wrap at 0xFFFF to ensure this is a 16 bit (2 byte) value.
+        return (specifiedAddress + offset) % 0xFFFF;
+      }
+      case AddressingMode.ZeroPage_Y: {
+        const specifiedAddress = this.memoryRead(this.program_counter);
+        const offset = this.register_y;
+        // Wrap at 0xFFFF to ensure this is a 16 bit (2 byte) value.
+        return (specifiedAddress + offset) % 0xFFFF;
+      }
+      case AddressingMode.Absolute_X: {
+        const specifiedAddress = this.memoryReadUint16(this.program_counter);
+        const offset = this.register_x;
+        // Wrap at 0xFFFF to ensure this is a 16 bit (2 byte) value.
+        return (specifiedAddress + offset) % 0xFFFF;
+      }
+      case AddressingMode.Absolute_Y: {
+        const specifiedAddress = this.memoryReadUint16(this.program_counter);
+        const offset = this.register_y;
+        // Wrap at 0xFFFF to ensure this is a 16 bit (2 byte) value.
+        return (specifiedAddress + offset) % 0xFFFF;
+      }
+      case AddressingMode.Indirect_X: {
+        const base = this.memoryRead(this.program_counter);
+        // Wrap at 0xFF so that this wraps around the zero page.
+        const ptr = (base + this.register_x) % 0xFF;
+        const lo = this.memoryRead(ptr);
+        // Wrap at 0xFFFF to ensure the address is a 16 bit (2 byte) value.
+        const hi = this.memoryRead((ptr + 1) % 0xFFFF);
+        return (hi << 8) | lo;
+      }
+      case AddressingMode.Indirect_Y: {
+        const base = this.memoryRead(this.program_counter);
+        const lo = this.memoryRead(base);
+        // Wrap at 0xFFFF to ensure the address is a 16 bit (2 byte) value.
+        const hi = this.memoryRead((base + 1) % 0xFFFF);
+        const deref_base = (hi << 8) | lo;
+        // Wrap at 0xFFFF to ensure this is a 16 bit (2 byte) value.
+        const deref = (deref_base + this.register_y) % 0xFFFF;
+        return deref;
+      }
+      default:
+        throw new Error(`Mode ${mode} not supported!`);
+    }
   }
 
   private updateZeroAndNegativeFlags(result: number): void {
